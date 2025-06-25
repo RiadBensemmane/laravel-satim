f9884a70886679820be63b72bf3c7783fc5a5e93
RiadBensemmane <riad200339@gmail.com>
2025-06-25 14:39:05 +0100

    revert tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,45 +2,9 @@
 
 namespace LaravelSatim\Tests;
 
-use LaravelSatim\SatimServiceProvider;
-use Orchestra\Testbench\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->artisan('config:clear');
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            SatimServiceProvider::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('satim.username', 'test_username');
-        $app['config']->set('satim.password', 'test_password');
-        $app['config']->set('satim.terminal', 'test_terminal');
-        $app['config']->set('satim.api_url', 'https://test.satim.dz/payment/rest');
-        $app['config']->set('satim.timeout', 30);
-        $app['config']->set('satim.language', 'en');
-        $app['config']->set('satim.currency', 'DZD');
-    }
-
-    protected function defineEnvironment($app)
-    {
-        $app['config']->set('satim', [
-            'username' => 'test_username',
-            'password' => 'test_password',
-            'terminal' => 'test_terminal',
-            'api_url' => 'https://test.satim.dz/payment/rest',
-            'timeout' => 30,
-            'language' => 'en',
-            'currency' => 'DZD',
-        ]);
-    }
+    //
 }
